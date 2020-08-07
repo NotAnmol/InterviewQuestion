f3fd03e530b40d393819c62d4e9865061645ae52
Anmol Jain <creators@Anmols-MacBook-Pro.local>
2020-08-07 12:27:02 +0530

    QUESTION: PlayNextSong Question

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+
+
+func playNextSong(of currentSongID: Int, from playlist: [Int], shuffle: Bool) -> Int? {
+    //Complete the function
+    return nil
+}
+
+let currentSongID: Int = 40
+let playlist: [Int] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+
+let shuffle: Bool = false
+//let shuffle: Bool = true
+
+print(playNextSong(of: currentSongID, from: playlist, shuffle: shuffle))
+
